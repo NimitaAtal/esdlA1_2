b765536cf027cff5cb4c1c051f1c33de1ee2aab1
NimitaAtal <atal.nimita@gmail.com>
2016-08-30 08:57:13 +0530

    Different message.
    Change in the calling.

--- A1.cpp
+++ A1.cpp
@@ -1,5 +1,6 @@
-Hello World :)
-Greetings to fellow Earthiens!
+Calling from Mars!
+Greetings Fellow Earthiens.
+This message is for the members of this universe.
 With best wishes,
 Kar-Al.
 (Master of the Universe)
